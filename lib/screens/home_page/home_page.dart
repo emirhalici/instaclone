cc5aa57b71ca2b585320d149b441dbd8ee92f8c3
emirhalici <emir.halici1@gmail.com>
2022-04-26 16:06:51 +0300

    implement home page appbar

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instaclone/utils/project_constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,21 +16,71 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     // animate to page
-    // pageController.animateToPage(1, duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
+
+    Color primaryColor = ProjectConstants.getPrimaryColor(context, false);
+    Color primaryColorReversed = ProjectConstants.getPrimaryColor(context, true);
 
     return SafeArea(
-      child: Scaffold(
-        body: PageView(
-          controller: pageController,
-          children: const [
-            Scaffold(
-              body: Text('Home Page!'),
+      child: PageView(
+        controller: pageController,
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              shape: Border(
+                bottom: BorderSide(
+                  color: primaryColor.withOpacity(0.2),
+                  width: 0.5,
+                ),
+              ),
+              backgroundColor: primaryColorReversed,
+              foregroundColor: primaryColor,
+              toolbarHeight: ProjectConstants.toolbarHeight,
+              title: SvgPicture.asset(
+                "assets/icons/logo.svg",
+                color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    // TODO : IMPLEMENT ADD POST SCREEN
+                  },
+                  icon: SvgPicture.asset(
+                    'assets/icons/add.svg',
+                    color: primaryColor,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    // TODO : IMPLEMENT RECENTLY MENU
+                  },
+                  icon: SvgPicture.asset(
+                    'assets/icons/heart.svg',
+                    color: primaryColor,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    pageController.animateToPage(1, duration: const Duration(milliseconds: 400), curve: Curves.ease);
+                  },
+                  icon: SvgPicture.asset(
+                    'assets/icons/messenger.svg',
+                    color: primaryColor,
+                  ),
+                ),
+              ],
+              centerTitle: false,
             ),
-            Scaffold(
-              body: Text("Messages"),
-            )
-          ],
-        ),
+            body: Column(
+              children: [
+                // TODO : IMPLEMENT STORIES
+                
+              ],
+            ),
+          ),
+          const Scaffold(
+            body: Text("Messages"),
+          )
+        ],
       ),
     );
   }
