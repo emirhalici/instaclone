70ca140344ede178c35abca6da536824e70ba528
emirhalici <emir.halici1@gmail.com>
2022-04-29 07:18:03 +0300

    fix overlapping bug and implement grid view

--- lib/screens/search_page/search_page.dart
+++ lib/screens/search_page/search_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instaclone/models/post_model.dart';
 import 'package:instaclone/providers/search_page_provider.dart';
 import 'package:instaclone/screens/post_page.dart';
@@ -15,6 +14,8 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchTextController = TextEditingController();
+
   Future<void> getSearchPosts() async {
     context.read<SearchPageProvider>().getPostsStreamForSearchPage();
   }
@@ -29,76 +30,81 @@ class _SearchPageState extends State<SearchPage> {
     }
 
     return SafeArea(
-      child: SingleChildScrollView(
-        controller: context.read<SearchPageProvider>().searchPostsController,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: primaryColor.withOpacity(0.04),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchTextController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
                 ),
+                isDense: true,
+                prefixIcon: const Icon(Icons.search),
+                filled: true,
+                fillColor:
+                    Theme.of(context).brightness == Brightness.dark ? primaryColor.withOpacity(0.15) : primaryColor.withOpacity(0.05),
+                hintText: 'Search',
               ),
             ),
-            stream == null
-                ? Center(
-                    child: CircularProgressIndicator.adaptive(
-                      backgroundColor: primaryColor,
-                    ),
-                  )
-                : StreamBuilder<List<List<PostModel>>>(
-                    stream: stream,
-                    builder: (BuildContext context, AsyncSnapshot<List<List<PostModel>>> list) {
-                      if (list.hasError) {
-                        return const Center(
-                          child: Text('Something went wrong'),
-                        );
-                      }
-
-                      if (list.connectionState == ConnectionState.waiting) {
-                        return Center(
-                          child: CircularProgressIndicator.adaptive(
-                            backgroundColor: primaryColor,
-                          ),
-                        );
-                      }
-
-                      var data = list.data![0];
-
-                      List<Widget> postWidgets = [];
-                      List<Widget> postImages = [];
-                      for (var post in data) {
-                        PostWidget postWidget = PostWidget(post: post);
-                        postWidgets.add(postWidget);
-                        postImages.add(
-                          InkWell(
-                            onTap: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => PostPage(postWidget: postWidget)));
-                            },
-                            splashColor: Colors.black.withOpacity(0.2),
-                            hoverColor: Colors.black.withOpacity(0.2),
-                            focusColor: Colors.black.withOpacity(0.2),
-                            highlightColor: Colors.black.withOpacity(0.2),
-                            child: Ink.image(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(post.pictures[0]),
-                              width: MediaQuery.of(context).size.width / 3,
-                              height: MediaQuery.of(context).size.width / 3,
-                            ),
-                          ),
-                        );
-                      }
-
-                      return StaggeredGrid.count(
-                        crossAxisCount: 3,
-                        children: postImages,
+          ),
+          stream == null
+              ? Center(
+                  child: CircularProgressIndicator.adaptive(
+                    backgroundColor: primaryColor,
+                  ),
+                )
+              : StreamBuilder<List<List<PostModel>>>(
+                  stream: stream,
+                  builder: (BuildContext context, AsyncSnapshot<List<List<PostModel>>> list) {
+                    if (list.hasError) {
+                      return const Center(
+                        child: Text('Something went wrong'),
                       );
-                    },
-                  )
-          ],
-        ),
+                    }
+
+                    if (list.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator.adaptive(
+                          backgroundColor: primaryColor,
+                        ),
+                      );
+                    }
+
+                    var data = list.data![0];
+
+                    List<Widget> postWidgets = [];
+                    List<Widget> postImages = [];
+                    for (var post in data) {
+                      PostWidget postWidget = PostWidget(post: post);
+                      postWidgets.add(postWidget);
+                      postImages.add(Material(
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => PostPage(postWidget: postWidget)));
+                          },
+                          highlightColor: Colors.black.withOpacity(0.2),
+                          child: Ink.image(
+                            image: NetworkImage(
+                              post.pictures[0],
+                            ),
+                            width: MediaQuery.of(context).size.width / 3,
+                            height: MediaQuery.of(context).size.width / 3,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ));
+                    }
+                    return GridView.count(
+                      crossAxisCount: 3,
+                      shrinkWrap: true,
+                      children: postImages,
+                    );
+                  },
+                )
+        ],
       ),
     );
   }
