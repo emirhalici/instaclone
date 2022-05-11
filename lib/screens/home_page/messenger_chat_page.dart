226de7b8026bda457474625c0392756e27c117fb
emirhalici <emir.halici1@gmail.com>
2022-05-12 01:58:38 +0300

    implement send message functionality

--- lib/screens/home_page/messenger_chat_page.dart
+++ lib/screens/home_page/messenger_chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:instaclone/models/chat_model.dart';
@@ -24,15 +25,18 @@ class _MessengerChatPageState extends State<MessengerChatPage> {
     super.initState();
   }
 
+  TextEditingController userMessageTextController = TextEditingController();
+  String userMessageText = '';
+
   @override
   Widget build(BuildContext context) {
+    void showSnackbarMessage(String message) => ProjectUtils.showSnackBarMessage(context, message);
+
     Color primaryColor = ProjectConstants.getPrimaryColor(context, false);
     Color primaryColorReversed = ProjectConstants.getPrimaryColor(context, true);
     var stream = context.watch<HomePageProvider>().specifiedChatStream;
 
-    print('document id: ${widget.documentId}');
     if (stream == null) {
-      print('stream is null');
       return Center(
         child: CircularProgressIndicator.adaptive(
           backgroundColor: primaryColor,
@@ -49,7 +53,6 @@ class _MessengerChatPageState extends State<MessengerChatPage> {
           }
 
           if (snapshot.connectionState == ConnectionState.waiting) {
-            print('connection is waiting');
             return Center(
               child: CircularProgressIndicator.adaptive(
                 backgroundColor: primaryColor,
@@ -75,145 +78,211 @@ class _MessengerChatPageState extends State<MessengerChatPage> {
             chatTitleText = users.join(', ');
           }
 
-          return Scaffold(
-            appBar: AppBar(
-              automaticallyImplyLeading: true,
-              backgroundColor: primaryColorReversed,
-              foregroundColor: primaryColor,
-              toolbarHeight: ProjectConstants.toolbarHeight,
-              centerTitle: false,
-              leadingWidth: 26,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    foregroundImage: (snapshot.data == null ||
-                            snapshot.data!.users == null ||
-                            snapshot.data!.users!.isEmpty ||
-                            snapshot.data!.users![0].profilePic == '')
-                        ? const AssetImage('assets/images/default_profile_pic.png') as ImageProvider
-                        : CachedNetworkImageProvider(snapshot.data!.users![0].profilePic),
-                    radius: 16,
-                  ),
-                  const SizedBox(width: 8),
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          chatTitleText,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
+          return GestureDetector(
+            onTap: () => WidgetsBinding.instance.focusManager.primaryFocus?.unfocus(),
+            child: Scaffold(
+              appBar: AppBar(
+                automaticallyImplyLeading: true,
+                backgroundColor: primaryColorReversed,
+                foregroundColor: primaryColor,
+                toolbarHeight: ProjectConstants.toolbarHeight,
+                centerTitle: false,
+                leadingWidth: 26,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      foregroundImage: (snapshot.data == null ||
+                              snapshot.data!.users == null ||
+                              snapshot.data!.users!.isEmpty ||
+                              snapshot.data!.users![0].profilePic == '')
+                          ? const AssetImage('assets/images/default_profile_pic.png') as ImageProvider
+                          : CachedNetworkImageProvider(snapshot.data!.users![0].profilePic),
+                      radius: 16,
                     ),
-                  )
-                ],
-              ),
-            ),
-            body: ListView.builder(
-              shrinkWrap: true,
-              reverse: false,
-              //physics: const ClampingScrollPhysics(),
-              itemCount: snapshot.data?.messages.length,
-              itemBuilder: ((context, index) {
-                if (index == 0) {
-                  print('item count: ${snapshot.data?.messages.length}');
-                }
-                MessageModel? message = snapshot.data?.messages[index];
-                bool isMe = message?.senderUUID == context.read<HomePageProvider>().userModel?.userUUID;
-
-                List<Widget> widgets = [];
-                if (index == 0) {
-                  DateTime date = message!.timestamp.toDate();
-                  String dateString =
-                      ProjectUtils.isDateToday(date) ? ProjectUtils.dateTimeToString(date) : ProjectUtils.dateTimeToStringWithDate(date);
-                  widgets.add(
-                    Center(
-                      child: Text(
-                        dateString,
-                        style: ProjectConstants.chatMessagesDateTextStyle(context),
-                      ),
-                    ),
-                  );
-                } else {
-                  DateTime date1 = message!.timestamp.toDate();
-                  DateTime date2 = snapshot.data!.messages[index - 1].timestamp.toDate();
-                  int diffInSeconds = ProjectUtils.timeDifferenceInSeconds(date1, date2);
-                  if (diffInSeconds > 60) {
-                    String dateString = ProjectUtils.isDateToday(date1)
-                        ? ProjectUtils.dateTimeToString(date1)
-                        : ProjectUtils.dateTimeToStringWithDate(date1);
-                    widgets.add(
-                      Center(
-                        child: Text(
-                          dateString,
-                          style: TextStyle(
-                            color: primaryColor.withOpacity(0.5),
-                            fontSize: 12.sp,
-                          ),
-                        ),
-                      ),
-                    );
-                  }
-                }
-                UserModel? user = isMe
-                    ? context.read<HomePageProvider>().userModel
-                    : snapshot.data?.users?.firstWhere((element) => element.userUUID == message.senderUUID);
-
-                if (isMe) {
-                  widgets.add(
-                    Align(
-                      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Material(
-                          elevation: 0,
-                          borderRadius: const BorderRadius.all(Radius.circular(22.0)),
-                          color: ProjectConstants.blueColor,
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Text(message.message),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                } else {
-                  widgets.add(
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                    const SizedBox(width: 8),
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          CircleAvatar(
-                            foregroundImage: (user == null || user.profilePic == '')
-                                ? const AssetImage('assets/images/default_profile_pic.png') as ImageProvider
-                                : CachedNetworkImageProvider(user.profilePic),
-                            radius: 16,
-                          ),
-                          Flexible(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Material(
-                                elevation: 0,
-                                borderRadius: const BorderRadius.all(Radius.circular(22.0)),
-                                color: primaryColor.withOpacity(0.15),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(12.0),
-                                  child: Flexible(child: Text(message.message)),
-                                ),
-                              ),
-                            ),
+                          Text(
+                            chatTitleText,
+                            overflow: TextOverflow.ellipsis,
                           ),
                         ],
                       ),
+                    )
+                  ],
+                ),
+              ),
+              body: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: false,
+                      reverse: false,
+                      itemCount: snapshot.data?.messages.length,
+                      itemBuilder: ((context, index) {
+                        MessageModel? message = snapshot.data?.messages[index];
+                        bool isMe = message?.senderUUID == context.read<HomePageProvider>().userModel?.userUUID;
+
+                        List<Widget> widgets = [];
+                        if (index == 0) {
+                          DateTime date = message!.timestamp.toDate();
+                          String dateString = ProjectUtils.isDateToday(date)
+                              ? ProjectUtils.dateTimeToString(date)
+                              : ProjectUtils.dateTimeToStringWithDate(date);
+                          widgets.add(
+                            Center(
+                              child: Text(
+                                dateString,
+                                style: ProjectConstants.chatMessagesDateTextStyle(context),
+                              ),
+                            ),
+                          );
+                        } else {
+                          DateTime date1 = message!.timestamp.toDate();
+                          DateTime date2 = snapshot.data!.messages[index - 1].timestamp.toDate();
+                          int diffInSeconds = ProjectUtils.timeDifferenceInSeconds(date1, date2);
+                          if (diffInSeconds > 60) {
+                            String dateString = ProjectUtils.isDateToday(date1)
+                                ? ProjectUtils.dateTimeToString(date1)
+                                : ProjectUtils.dateTimeToStringWithDate(date1);
+                            widgets.add(
+                              Center(
+                                child: Text(
+                                  dateString,
+                                  style: TextStyle(
+                                    color: primaryColor.withOpacity(0.5),
+                                    fontSize: 12.sp,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                        }
+                        UserModel? user = isMe
+                            ? context.read<HomePageProvider>().userModel
+                            : snapshot.data?.users?.firstWhere((element) => element.userUUID == message.senderUUID);
+                        if (isMe) {
+                          widgets.add(
+                            Align(
+                              alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Material(
+                                  elevation: 0,
+                                  borderRadius: const BorderRadius.all(Radius.circular(22.0)),
+                                  color: ProjectConstants.blueColor,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: Text(message.message),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        } else {
+                          widgets.add(
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  CircleAvatar(
+                                    foregroundImage: (user == null || user.profilePic == '')
+                                        ? const AssetImage('assets/images/default_profile_pic.png') as ImageProvider
+                                        : CachedNetworkImageProvider(user.profilePic),
+                                    radius: 16,
+                                  ),
+                                  Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Material(
+                                        elevation: 0,
+                                        borderRadius: const BorderRadius.all(Radius.circular(22.0)),
+                                        color: primaryColor.withOpacity(0.15),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(12.0),
+                                          child: Text(message.message),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }
+                        return Column(
+                          children: widgets,
+                        );
+                      }),
                     ),
-                  );
-                }
-                return Column(
-                  children: widgets,
-                );
-              }),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 8.w),
+                    child: TextField(
+                      controller: userMessageTextController,
+                      textAlignVertical: TextAlignVertical.center,
+                      onChanged: (value) {
+                        setState(() {
+                          userMessageText = value;
+                        });
+                      },
+                      minLines: 1,
+                      maxLines: 2,
+                      expands: false,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        hintText: 'Message',
+                        contentPadding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 26.0),
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(24.0)),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(24.0)),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(24.0)),
+                        ),
+                        suffix: GestureDetector(
+                          onTap: () async {
+                            if (userMessageText == '') {
+                              return;
+                            }
+                            snapshot.data?.messages.add(
+                              MessageModel(
+                                message: userMessageText,
+                                senderUUID: context.read<HomePageProvider>().userModel?.userUUID as String,
+                                timestamp: Timestamp.now(),
+                              ),
+                            );
+                            bool response = await context.read<HomePageProvider>().writeToChatModel(snapshot.data!);
+                            if (!response) {
+                              showSnackbarMessage('Failed to send message');
+                            } else {
+                              userMessageTextController.clear();
+                              userMessageText = '';
+                            }
+                          },
+                          child: Text(
+                            'Send  ',
+                            style: TextStyle(
+                              color: userMessageText == '' ? ProjectConstants.blueColor.withAlpha(100) : ProjectConstants.blueColor,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        fillColor: primaryColor.withOpacity(0.15),
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
